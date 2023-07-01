fb3ab982690f2f8a45543333bb802eddf3c911a3
VaibhavGupta911 <guptaayush519@gmail.com>
2023-07-01 17:58:53 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useReducer, useRef ,useState} from 'react'
+import { useEffect, useReducer, useRef, useState } from 'react'
 import './App.css';
 import useConsoleLog from "./useConsoleLog";
 function App() {
 
-
+  //usereducer
   const calculator = (state, action) => {
     if (action.type === "buy") return { money: state.money - 10 };
     if (action.type === "sell") return { money: state.money + 10 };
@@ -18,12 +18,39 @@ function App() {
     focusRef.current.focus();
   }
 
-///////////////////
-const [count,setCount]=useState(0);
-useConsoleLog(count)
-const pLus=()=>setCount(preCount=>preCount+1);
+  ///custom hook
+  const [count, setCount] = useState(0);
+  useConsoleLog(count)
+  const pLus = () => setCount(preCount => preCount + 1);
 
-
+  ////Custom Hook 
+  const [day, setDay] = useState("Monday");
+  const prevDay = usePrevious(day);
+  const getNextDay = () => {
+    if (day === "Monday") {
+      setDay("Tuesday")
+    } else if (day === "Tuesday") {
+      setDay("Wednesday")
+    } else if (day === "Wednesday") {
+      setDay("Thursday")
+    } else if (day === "Thursday") {
+      setDay("Friday")
+    } else if (day === "Friday") {
+      setDay("Monday")
+    }
+  }
+  function usePrevious(val) {
+    const ref = useRef()
+    useEffect(
+      () => { ref.current = val },{/*We are making ref as useref 
+                                  then assining var as its current value
+                                 then retuning ref current to prevday
+                                also useeffect will run when var change
+                              */}
+      [val]
+    )
+    return ref.current
+  }
   return (
     <div className="App">
       {/*useREDUCER Example */}
@@ -32,12 +59,26 @@ const pLus=()=>setCount(preCount=>preCount+1);
       <button onClick={() => despatch({ type: 'sell' })}>SELL DISH</button>
 
       {/*useREF Example */}
-      <input type='text' ref={focusRef}/>
+      <input type='text' ref={focusRef} />
       <button onClick={focusFun}>For focus on input field</button>
 
       {/*CUSTOM HOOK Example */}
       <h1>Count: {count}</h1>
       <button onClick={pLus}>Plus</button>
+
+
+      {/*CUSTOM HOOK Example */}
+      <h1>
+        Today is: {day}<br />
+        {
+          prevDay && (
+            <span>Previous work day was: {prevDay}</span>
+          )
+        }
+      </h1>
+      <button onClick={getNextDay}>
+        Get next day
+      </button>
     </div>
   );
 }
